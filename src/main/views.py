9f5301fe63e61f9a84a1d6c7582788ed2433cdcc
Alexander <qazwsxedc19909@gmail.com>
2020-07-25 20:02:55 +0300

    Moved torrent_detail jsons. Updated/fixed list

--- src/main/views.py
+++ src/main/views.py
@@ -27,7 +27,7 @@ class MovieListView(ListView):
     model = Movie
     queryset = Movie.objects.filter(
         Q(download_count__gte=10000) & Q(rating__gte=7) & Q(year__gte=1995)).order_by('-rating')
-    paginate_by = 8
+    paginate_by = 4
     template_name = 'main/movie_list.html'
 
     def all_filtered_ids(self, object_list=queryset):
@@ -56,28 +56,24 @@ class MovieListView(ListView):
                 continue
         return movies
 
-    # def random_movies(self):
-    #     # TODO right now these are only random movies without 7+ validation
-    #     max_id = Movie.objects.all().aggregate(max_id=Max("id"))['max_id']
-    #     min_id = Movie.objects.all().aggregate(min_id=Min("id"))['min_id']
-    #     iterations = 0
-    #     stored_movies = []
-    #     while iterations < self.paginate_by:
-    #         rand = random.randint(min_id, max_id)
-    #         # If we have this movie
-    #         try:
-    #             m = Movie.objects.get(id=rand)
-    #             # This way of validation is shitty. It doubles number of queries
-    #             stored_movies.append(m)
-    #             iterations += 1
-    #         # In case we don't have this movie
-    #         except:
-    #             continue
-    #     return stored_movies
+    def random_movies(self):
+        ids = self.all_filtered_ids()
+        stored_movies = []
+        iterations = 0
+        while iterations < self.paginate_by:
+            rand = random.choice(ids)
+            # If we have this movie
+            try:
+                m = Movie.objects.get(id=rand)
+                stored_movies.append(m)
+                iterations += 1
+            except:
+                continue
+        return stored_movies
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        # context['stored_movies'] = self.random_movies()
+        context['stored_movies'] = self.random_movies()
         context['random_popular_movies'] = self.random_popular_movies()
         return context
 
@@ -85,7 +81,7 @@ class MovieListView(ListView):
 class LatestMoviesView(ListView):
     queryset = Movie.objects.all()
     template_name = 'main/latest_movies.html'
-    paginate_by = 16
+    paginate_by = 12
 
 
 class CastMoviesList(DetailView):
@@ -95,7 +91,7 @@ class CastMoviesList(DetailView):
 
 
 class SearchResultsView(ListView):
-    model = Movie
+    # model = Movie
     template_name = 'search_results.html'
     paginate_by = 16
 
