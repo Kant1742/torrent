ab6798ffea75ff9861fba75ea3fac914886e6ad8
Alexander <qazwsxedc19909@gmail.com>
2020-07-27 17:25:44 +0300

    deploy files

--- src/torrent/settings.py
+++ src/torrent/settings.py
@@ -60,6 +60,7 @@ INTERNAL_IPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
