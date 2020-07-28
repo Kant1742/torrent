dc1626025dda16a81ca18155b5c3250f7c0c2f1a
Alexander <qazwsxedc19909@gmail.com>
2020-07-28 09:25:23 +0300

    Data/heroku relic

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn torrent.wsgi --log-file -
+web: newrelic-admin run-program gunicorn torrent.wsgi --log-file -
--- torrent/settings.py
+++ torrent/settings.py
@@ -12,6 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "zw1fb1*xss2m+gvz#1+(xnn0%v3ss)ax12@y_t#+_@*=aa@%+_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'subov-net.herokuapp.com',
