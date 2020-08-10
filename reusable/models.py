1dd663a86423bb219a2e2aae33bc08e6fa3377ad
Alexander <qazwsxedc19909@gmail.com>
2020-08-10 17:46:34 +0300

    Some changes, data

--- reusable/models.py
+++ reusable/models.py
@@ -14,7 +14,7 @@ class ReusableFields(models.Model):
     title = models.CharField(max_length=100, null=True, blank=True)
     slug = models.SlugField(max_length=100, unique=True, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    image = models.URLField(blank=True, null=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.title
